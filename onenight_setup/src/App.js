6efc6ccedfe3546c21d2819e40fd55e91f6e1b65
mschwar7z <mschwar7z@gmail.com>
2025-05-18 19:23:36 -0700

    New Header

--- onenight_setup/src/App.js
+++ onenight_setup/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header.js';
 
 function App() {
   const handleNameChange = () => {
@@ -10,20 +11,8 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello {handleNameChange()}!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      
     </div>
   );
 }
--- onenight_setup/src/Header.js
+++ onenight_setup/src/Header.js
@@ -1,1 +1,11 @@
-<></>
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <h1>Mia's List</h1>
+    </header>
+  )
+}
+
+export default Header
